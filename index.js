52db478dba73116bebc533c500dda135a0520e3c
Hala AlArid <halaalarid99@gmail.com>
2022-10-24 00:52:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "John", 
+    streetAddress: "Main Street"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {
+        name: "Sam",
+        streetAddress: "11 Broadway"
+    }
+
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    
+}
+
+function deleteFromEmployeeByKey(employee, key){
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+}
